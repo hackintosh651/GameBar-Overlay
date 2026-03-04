de41c42b39dd6eac9115543267cc961c2311c448
hackintosh651 <bojangles.cco@gmail.com>
2026-03-04 07:39:46 -0500

    Update mouse unlock.js

--- addons/mouse unlock.js
+++ addons/mouse unlock.js
@@ -1,1 +1,9 @@
+ // Create the close button widget
+        this._unlockmButton = new St.Button({
+            style_class: 'gamebar-close-button', // CSS class for styling
+            child: new St.Icon({ icon_name: 'window-close-symbolic' }) // Icon for the close button
+        });
 
+        // Hide the overlay when the close button is clicked
+        this._unlockm.connect('clicked', () => {
+            this._exitPointerLock();
