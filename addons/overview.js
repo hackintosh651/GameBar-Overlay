1f1dface8bdb3069d8f5e1c295c1e8b7424acd9d
hackintosh651 <bojangles.cco@gmail.com>
2026-03-04 10:41:42 -0500

    Update overview.js

--- addons/overview.js
+++ addons/overview.js
@@ -1,4 +1,20 @@
- // Create the close button widget
+import St from 'gi://St';
+import GLib from 'gi://GLib';
+import Clutter from 'gi://Clutter';
+import { getPositionStyle } from '../utils.js';
+
+export class Clock {
+  constructor(overlay, primaryMonitor) {
+    this._overlay = overlay;
+    this._primaryMonitor = primaryMonitor;
+    this._widthChangeId = null;
+    this._heightChangeId = null;
+    this._timeLabel = null;
+    this._timeoutId = null;
+    this._addonContainer = null;
+    this._visibilityChangedId = null;
+    this._createClockWidget();
+  }
         this._unlockmButton = new St.Button({
             style_class: 'gamebar-close-button', // CSS class for styling
             child: new St.Icon({ icon_name: 'window-close-symbolic' }) // Icon for the close button
